e71c365dd3969f644069fe48d9fce667104a1089
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2008-09-04 19:06:00 +0000

    update KontorX

---
+++ File.php
@@ -0,0 +1,9 @@
+<?php
+class KontorX_File implements KontorX_File_Interface {
+	public function __construct($pathname = null) {
+		
+	}
+	public function save($data);
+	public function delete($data);
+}
+?>
