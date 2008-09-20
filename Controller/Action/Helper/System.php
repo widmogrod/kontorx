70fdef6715364bc8004824a7da19f4fb94e270ad
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2008-09-20 15:06:15 +0000

    KontorX_Controller helper, plugin - systemowy - stabilne api i funkcjonalne!

--- Controller/Action/Helper/System.php
+++ Controller/Action/Helper/System.php
@@ -3,9 +3,45 @@ require_once 'Zend/Controller/Action/Helper/Abstract.php';
 class KontorX_Controller_Action_Helper_System extends Zend_Controller_Action_Helper_Abstract {
 
     public function init() {
+		$action  = $this->getActionController();
+		$request = $action->getRequest();
+		$plugin  = $this->getPluginInstance();
 
+		// setup template
+		if (isset($action->skin)) {
+			if (is_array($action->skin)) {
+				$options = $action->skin;
+
+				// sprawdz czy nazwa akcji nie jest zastrzezona
+				// i czy istnieje jako oddzielna konfiguracja skórki dla akcji
+				$actionName = $request->getActionName();
+				if (!in_array($actionName, array('layout', 'dynamic', 'template'))
+						&& array_key_exists($actionName, $options)) {
+					$options = $action->skin[$actionName];
+				}
+
+				// layout name
+				if (isset($options['layout'])) {
+					$plugin->setLayoutName($options['layout']);
+				} else
+				// dynamic layout name
+				if (isset($options['dynamic'])) {
+					$dynamicName = $request->getControllerName() . '_' . $request->getActionName();
+					$plugin->setLayoutName($dynamicName);
+				}
+
+				// template name
+				if (isset($options['template'])) {
+					$plugin->setTemplateName($options['template']);
+				}
+			} else
+			if (is_string($action->skin)){
+				// template name
+				$this->getPluginInstance()->setTemplateName($action->skin);
+			}
+		}
     }
-
+    
     /**
      * @return KontorX_Controller_Plugin_System
      */
@@ -27,24 +63,32 @@ class KontorX_Controller_Action_Helper_System extends Zend_Controller_Action_Hel
 			if (!$front->hasPlugin('KontorX_Controller_Plugin_System')) {
 				throw new Zend_Controller_Exception('Plugin `KontorX_Controller_Plugin_System` is no exsists');
 			}
-			$this->_pluginInstance = $fron->getPlugin('KontorX_Controller_Plugin_System');
+			$this->_pluginInstance = $front->getPlugin('KontorX_Controller_Plugin_System');
 		}
 		return $this->_pluginInstance;
 	}
-	
+
 	/**
-	 * @return KontorX_Controller_Action_Helper_System
+	 * Ustawia instancje obiektu
+	 *
+	 * @param KontorX_Controller_Plugin_System $plugin
+	 */
+	public function setPluginInstance(KontorX_Controller_Plugin_System $plugin) {
+		$this->_pluginInstance = $plugin;
+	}
+
+	/**
+	 * @return string
 	 */
 	public  function language() {
-		$this->getPluginInstance()->getLanguage();
-		return $this;
+		return $this->getPluginInstance()->getLanguage();
 	}
 
 	/**
 	 * @return KontorX_Controller_Action_Helper_System
 	 */
     public function template($template) {
-        $this->getPluginInstance()->setTemplate($template);
+        $this->getPluginInstance()->setTemplateName($template);
         return $this;
     }
 
@@ -52,7 +96,43 @@ class KontorX_Controller_Action_Helper_System extends Zend_Controller_Action_Hel
 	 * @return KontorX_Controller_Action_Helper_System
 	 */
     public function layout($layout) {
-    	$this->getPluginInstance()->setLayout($layout);
+    	$this->getPluginInstance()->setLayoutName($layout);
     	return $this;
     }
+
+    /**
+     * Dodaje do include path sciezkę do katalogu z modelem
+     *
+     * @return KontorX_Controller_Action_Helper_System
+     */
+    public function addModelIncludePath() {
+    	$action = $this->getActionController();
+    	$request = $action->getRequest();
+    	// 
+    	$this->_addModuleIncludeDirname($request->getModuleName(), 'models');
+    	
+    	return $this;
+    }
+
+	/**
+	 * Dodaje do "include_path" określone położenie w module!
+	 *
+	 * @param string $module
+	 * @param string $dirname
+	 */
+	protected function _addModuleIncludeDirname($module, $dirname) {
+		$applicationPath = $this->getPluginInstance()->getApplicationPath(
+			KontorX_Controller_Plugin_System::APPLICATION_MODULES_DIRNAME);
+
+		$path = $applicationPath . basename($module) . '/' . $dirname;
+
+		if (is_dir($path)) {
+			if (strstr(get_include_path(), $path) === false) {
+				set_include_path(
+					get_include_path() . PATH_SEPARATOR .
+					$path
+				);
+			}
+		}
+	}
 }
