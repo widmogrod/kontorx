86bc7faf9b084e6c59d0052872b1720f8a188bc9
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2008-12-29 10:38:26 +0000

    Update Table*Json

--- Db/Table/Rowset/Json.php
+++ Db/Table/Rowset/Json.php
@@ -2,19 +2,8 @@
 require_once 'Zend/Db/Table/Rowset/Abstract.php';
 class KontorX_Db_Table_Rowset_Json extends Zend_Db_Table_Rowset_Abstract {
 	
-    public function toArray(){
-        foreach ($this->_rows as $i => $row) {
-            $this->_data[(int) $i] = $row->toArray() + array('asd' => 'asdasd');
-        }
-        return $this->_data;
-    }
-	
 	public function toJson() {
 	    require_once 'Zend/Json.php';
 	    return Zend_Json::encode($this->toArray());
-	}
-
-	public function __toString() {
-		return $this->toJson();
 	}
 }
