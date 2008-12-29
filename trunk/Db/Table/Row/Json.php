bf5873a7a67be1abccd6ed24283348344fe970ad
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2008-12-29 10:38:26 +0000

    Update Table*Json

--- trunk/Db/Table/Row/Json.php
+++ trunk/Db/Table/Row/Json.php
@@ -5,8 +5,4 @@ class KontorX_Db_Table_Row_Json extends Zend_Db_Table_Row_Abstract {
 	    require_once 'Zend/Json.php';
 	    return Zend_Json::encode($this->toArray());
 	}
-
-	public function __toString() {
-		return $this->toJson();
-	}
 }
