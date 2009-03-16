7b63e4df4a8a0161f4b584dbc52f723bb1318f99
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-03-16 10:27:08 +0000

    update ...

--- View/Helper/BaseUrl.php
+++ View/Helper/BaseUrl.php
@@ -2,125 +2,129 @@
 /**
  * Helper widoku, umożliwia generowanie strokotry drzewiastej,
  * wykorzystywany jest dla "ExtJs"
- * 
+ *
  * @category 	KontorX
  * @package 	KontorX_View_Helper
  * @version 	0.1.2
  * @license		GNU GPL
  * @author 		Marcin `widmogror` Habryn, widmogrod@gmail.com
- * 
+ *
  * @todo		Dodać opisy
  */
 class KontorX_View_Helper_BaseUrl {
-	protected static $_url = array(); 
+    protected static $_url = array();
 
-	/**
-	 * Enter description here...
-	 *
-	 * @param boolean $scriptName
-	 * @return string
-	 */
-	public static function baseUrl($scriptName = false){
-		// TODO Dodac rozpoznawanie czy https
-		switch($scriptName){
-			case true:
-				$phpSelf = getenv('PHP_SELF');
-				$scriptName = ($phpSelf === false OR $phpSelf == '' OR $phpSelf == '/' )
-					? getenv('SCRIPT_NAME')
-					: $phpSelf;
-				break;
+    /**
+     * Enter description here...
+     *
+     * @param boolean $scriptName
+     * @return string
+     */
+    public static function baseUrl($scriptName = false) {
+        // TODO Dodac rozpoznawanie czy https
+        switch($scriptName) {
+            case true:
+                $phpSelf = getenv('PHP_SELF');
+                $scriptName = ($phpSelf === false OR $phpSelf == '' OR $phpSelf == '/' )
+                ? getenv('SCRIPT_NAME')
+                : $phpSelf;
+                break;
 
-			case false:
-				$scriptName = dirname(getenv('SCRIPT_NAME'));
-				$iScriptName = strlen($scriptName);
-				$scriptName = $scriptName{$iScriptName-1} == '/'
-					? substr($scriptName,0,-1)
-					: $scriptName;
-				$scriptName .= '/';
-				break;
-		}
+            case false:
+                $scriptName = dirname(getenv('SCRIPT_NAME'));
+                $iScriptName = strlen($scriptName);
+                $scriptName = $scriptName {$iScriptName-1} == '/'
+                ? substr($scriptName,0,-1)
+                : $scriptName;
+                $scriptName .= '/';
+                break;
+        }
 
-		return 'http://' . getenv('SERVER_NAME') . $scriptName;
-	}
+        return 'http://' . getenv('SERVER_NAME') . $scriptName;
+    }
 
-	/**
-	 * Enter description here...
-	 *
-	 * @param bool $scriptName
-	 * @return string
-	 */
-	public static function getUrl($scriptName = false) {
-		if (array_key_exists((int) $scriptName, self::$_url)) {
-			return self::$_url[(int) $scriptName];
-		}
+    public function __toString() {
+        // TODO
+        return '';
+    }
 
-		switch($scriptName){
-			case true:
-				$url = ($scriptName = getenv('SCRIPT_NAME')) === false ? getenv('REQUEST_URI') : $scriptName;
-				break;
-			case false:
-				if (($pathInfo = getenv('PATH_INFO')) !== false){
-					$url = $pathInfo;
-				} else {
-					$requestUri = getenv('REQUEST_URI');
-					$scriptName = getenv('SCRIPT_NAME');
-					$dirnameScriptName = dirname($scriptName);
+    /**
+     * Enter description here...
+     *
+     * @param bool $scriptName
+     * @return string
+     */
+    public static function getUrl($scriptName = false) {
+        if (array_key_exists((int) $scriptName, self::$_url)) {
+            return self::$_url[(int) $scriptName];
+        }
 
-					$url = ( $scriptName == '/' OR $dirnameScriptName == '/')
-						? $requestUri
-						: str_replace(array($scriptName, $dirnameScriptName),'',$requestUri);
-				}
-				
-				$filename = basename(getenv('SCRIPT_FILENAME'));
-				$url = str_replace($filename,'',$url);
-				
-				if(($scriptName = getenv('QUERY_STRING')) != '') {
-					$url .= '?'.$scriptName;
-				}
-				break;
-		}
+        switch($scriptName) {
+            case true:
+                $url = ($scriptName = getenv('SCRIPT_NAME')) === false ? getenv('REQUEST_URI') : $scriptName;
+                break;
+            case false:
+                if (($pathInfo = getenv('PATH_INFO')) !== false) {
+                    $url = $pathInfo;
+                } else {
+                    $requestUri = getenv('REQUEST_URI');
+                    $scriptName = getenv('SCRIPT_NAME');
+                    $dirnameScriptName = dirname($scriptName);
 
-		return self::$_url[(int) $scriptName] = urldecode($url);
-	}
+                    $url = ( $scriptName == '/' OR $dirnameScriptName == '/')
+                    ? $requestUri
+                    : str_replace(array($scriptName, $dirnameScriptName),'',$requestUri);
+                }
 
-	public static function getDomain() {
-		return 'http://' . getenv('SERVER_NAME') . '/';
-	}
-	
-	/**
-	 * Enter description here...
-	 *
-	 * @param string $string
-	 * @param bool $scriptName
-	 * @return bool
-	 */
-	public static function inUrl($string, $true = null, $false = null, $scriptName = false, array $noIn = array()) {
-		$url = self::getUrl($scriptName);
-		$inUrl = strstr($url, $string);
+                $filename = basename(getenv('SCRIPT_FILENAME'));
+                $url = str_replace($filename,'',$url);
 
-		$return = $inUrl === false
-			? (null === $false ? false : $false)
-			: (null === $true ? true : $true);
+                if(($scriptName = getenv('QUERY_STRING')) != '') {
+                    $url .= '?'.$scriptName;
+                }
+                break;
+            }
 
-		if (!$inUrl) {
-			return $return;
-		}
+        return self::$_url[(int) $scriptName] = urldecode($url);
+    }
 
-		foreach ($noIn as $value) {
-			if(strstr($url, $value) !== false) {
-				return null === $false ? false : $false;
-			}
-		}
-		return $return;
-	}
+    public static function getDomain() {
+        return 'http://' . getenv('SERVER_NAME') . '/';
+    }
 
-	/**
-	 * Enter description here...
-	 *
-	 * @return string
-	 */
-	public static function getActivePageUrl() {
-		return self::getDomain() . self::getUrl();
-	}
+    /**
+     * Enter description here...
+     *
+     * @param string $string
+     * @param bool $scriptName
+     * @return bool
+     */
+    public static function inUrl($string, $true = null, $false = null, $scriptName = false, array $noIn = array()) {
+        $url = self::getUrl($scriptName);
+        $inUrl = strstr($url, $string);
+
+        $return = $inUrl === false
+        ? (null === $false ? false : $false)
+        : (null === $true ? true : $true);
+
+        if (!$inUrl) {
+            return $return;
+        }
+
+        foreach ($noIn as $value) {
+            if(strstr($url, $value) !== false) {
+                return null === $false ? false : $false;
+            }
+        }
+        return $return;
+    }
+
+    /**
+     * Enter description here...
+     *
+     * @return string
+     */
+    public static function getActivePageUrl() {
+        return self::getDomain() . self::getUrl();
+    }
 }
-?>
