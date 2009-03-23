fe9f371c44caa71327da0d0395f0aa7b063add6c
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-03-23 13:06:27 +0000

    Search_Semantic_Content

---
+++ KontorX/Search/Semantic/Context/Interface.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author gabriel
+ *
+ */
+interface KontorX_Search_Semantic_Context_Interface extends  Iterator, Countable {
+	/**
+	 * @return string
+	 */
+	public function getInput();
+	
+	/**
+	 * @param string $input
+	 * @return void
+	 */
+	public function setInput($input);
+	
+	/**
+	 * @param mixed $data
+	 * @return void
+	 */
+	public function setOutput($data);
+	
+	/**
+	 * @param string $name
+	 * @param mixed $data
+	 * @return void
+	 */
+	public function addOutput($name, $data);
+	
+	/**
+	 * @param string $name
+	 * @return array|null
+	 */
+	public function getOutput($name = null);
+}
