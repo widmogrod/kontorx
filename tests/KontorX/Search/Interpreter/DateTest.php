f6dfe420152e5325a25465ff9aa8bd8dab3c2ad1
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-03-23 13:14:01 +0000

    Test for Search_Semantic_Interpreter

---
+++ tests/KontorX/Search/Interpreter/DateTest.php
@@ -0,0 +1,92 @@
+<?php
+require_once '../../../setupTest.php';
+
+/**
+ * @see KontorX_Search_Semantic_Query_Date 
+ */
+require_once 'KontorX/Search/Semantic/Interpreter/Date.php';
+
+/**
+ * @see KontorX_Search_Semantic_Context 
+ */
+require_once 'KontorX/Search/Semantic/Context.php';
+
+class KontorX_Search_Semantic_Interpreter_DateTest extends UnitTestCase {
+	
+	/**
+	 * @var KontorX_Search_Semantic_Interpreter_Date
+	 */
+	protected $_interpreter = null;
+	
+	public function setUp() {
+		$this->_interpreter = new KontorX_Search_Semantic_Interpreter_Date();
+	}
+	
+	public function tearDown() {
+		$this->_interpreter = null;
+	}
+
+	public function testDataDDMMYYYYInContent() {
+		$date = '11-03-2009';
+		$context = "Dzisiaj jest $date";
+		$contextInstance = new KontorX_Search_Semantic_Context($context);
+
+		$this->_interpreter->interpret($contextInstance);
+		$data = $contextInstance->getOutput();
+		
+		$message = sprintf('Znaleziona data "%s" w tekscie "%s" jest różna od oczekiwanej "%s"', $data, $context, $date);
+		$this->assertEqual($data, $date, $message);
+    }
+
+	public function testTimeHHIISSInContent() {
+		$time = '13:12:59';
+		$context = "Jest godzina $time po południu";
+		$contextInstance = new KontorX_Search_Semantic_Context($context);
+
+		$this->_interpreter->interpret($contextInstance);
+		$data = $contextInstance->getOutput();
+		
+		$message = sprintf('Znaleziona godzina "%s" w tekscie "%s" jest różna od oczekiwanej "%s"', $data, $context, $time);
+		$this->assertEqual($data, $time, $message);
+    }
+
+	public function testTimeHHMMInContent() {
+		$time = '11:12';
+		$context = "Jest godzina $time po południu";
+		$contextInstance = new KontorX_Search_Semantic_Context($context);
+
+		$this->_interpreter->interpret($contextInstance);
+		$data = $contextInstance->getOutput();
+		
+		$message = sprintf('Znaleziona godzina "%s" w tekscie "%s" jest różna od oczekiwanej "%s"', $data, $context, $time);
+		$this->assertEqual($data, $time, $message);
+    }
+    
+	public function testTimeHHInContent() {
+		$time = '11';
+		$context = "Jest godzina $time po południu";
+		$contextInstance = new KontorX_Search_Semantic_Context($context);
+
+		$this->_interpreter->interpret($contextInstance);
+		$data = $contextInstance->getOutput();
+		
+		$message = sprintf('Znaleziona godzina "%s" w tekscie "%s" jest różna od oczekiwanej "%s"', $data, $context, $time);
+		$this->assertEqual($data, $time, $message);
+    }
+    
+	public function testNoTimeInContent() {
+		$time = 'o brak';
+		$correct = array();
+		$context = "Jest godzina $time po południu";
+		$contextInstance = new KontorX_Search_Semantic_Context($context);
+
+		$this->_interpreter->interpret($contextInstance);
+		$data = $contextInstance->getOutput();
+
+		$message = sprintf('Znaleziona godzina "%s" w tekscie "%s" jest różna od oczekiwanej "%s"', $data, $context, $correct);
+		$this->assertIdentical($data, $correct, $message);
+    }
+}
+
+$r = new KontorX_Search_Semantic_Interpreter_DateTest();
+$r->run(new TextReporter());
