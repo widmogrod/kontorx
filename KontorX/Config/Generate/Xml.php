9190e327dcdc3613c20a3f81837bc23472dc8b79
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-03-25 21:50:43 +0000

    Update Config_Generate_Xml

--- KontorX/Config/Generate/Xml.php
+++ KontorX/Config/Generate/Xml.php
@@ -6,18 +6,16 @@ require_once 'KontorX/Config/Generate/Abstract.php';
  * 
  * @category 	KontorX_Config
  * @package 	KontorX_Config_Generate
- * @version 	0.1.2
  * @license		GNU GPL
- * @author 		Marcin `widmogror` Habryn, widmogrod@gmail.com
+ * @author 		Gabriel gabriel@widmogrod.info
  * 
  * TODO dodac opcje
  *  - czy zapis z sekcjami
  *  - czy wprowadzic rozszerzenie [sekcja : rozszerzenie]
- * TODO optymalizacja klasa jest za wolna!!
  */
 class KontorX_Config_Generate_Xml extends KontorX_Config_Generate_Abstract {
 
-	protected $_rootnode = 'configdata';
+	const NUMERIC_KEY_PREFIX = 'kontorx_numeric_key_';
 
 	/**
 	 * Zwraca wygenerowany string z array lub @see Zend_Config
@@ -25,37 +23,35 @@ class KontorX_Config_Generate_Xml extends KontorX_Config_Generate_Abstract {
 	 * @return string
 	 */
 	public function generate() {
-		$rootnode = "<$this->_rootnode></$this->_rootnode>";
-		$xml = $this->_generate($this->getConfig(), new SimpleXMLElement($rootnode));
+		$xml = new SimpleXMLElement('<kontorx-config/>');
+		$xml = $this->_generate($this->getConfig(), $xml);
 
 		// generowanie pliku
-		$result  = "<!--" . self::INFO . "-->";
-		$result .= $xml->asXML();
-		return $result;
+		$dom = dom_import_simplexml($xml)->ownerDocument;
+        $dom->formatOutput = true;
+        
+		return $dom->saveXML();
 	}
 
 	/**
-	 * Enter description here...
-	 * 
-	 * //TODO Zeby dzialalo!
-	 * 
 	 * @param array $array
 	 * @param SimpleXMLElement $xml
 	 * @return SimpleXMLElement
 	 */
 	private function _generate(array $array, SimpleXMLElement $xml){
-		foreach ($array as $key_1 => $val_1) {
-			// zapis atrybutow
-			if (isset($val_1['@attributes']) && is_array($val_1['@attributes'])) {
-				$xmlNew = $xml->addChild($key_1);
-				foreach ($val_1['@attributes'] as $key_2 => $val_2) {
-					$xmlNew->addAttribute($key_2, $val_2);
-				}
-			} else
-			if (is_array($val_1)) {
-				$xml = $this->_generate($val_1, $xml);
+		foreach ($array as $key => $value) {
+			// Klucze numeryczne są niedozwolone
+			if (is_numeric($key)) {
+				$key = self::NUMERIC_KEY_PREFIX . $key;
+			}
+			
+			// @todo Zastanowić się czy nie parsować klucza jako alfa-numerycznego?
+
+			if (is_array($value)) {
+				$child = $xml->addChild($key);
+				$xml = $this->_generate($value, $child);
 			} else {
-				$xml->addChild($key_1, $val_1);
+				$xml->addChild($key, (string) $value);
 			}
 		}
 		return $xml;
