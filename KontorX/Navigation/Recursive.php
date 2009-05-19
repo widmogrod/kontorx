13f2db13ffeb073198bfb26fd0abf05051e74f09
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-05-19 09:16:56 +0000

    New: Navigation_Recursive, Navigation_Recursive_Visitor

---
+++ KontorX/Navigation/Recursive.php
@@ -0,0 +1,72 @@
+<?php
+class KontorX_Navigation_Recursive extends RecursiveIteratorIterator {
+	/**
+	 * @var Zend_Navigation_Container
+	 */
+	private $_navigation = null;
+
+	/**
+	 * @param Zend_Navigation_Container $navigation
+	 * @param Traversable $iterator
+	 * @param $mode
+	 * @param $flags
+	 * @return void
+	 */
+	public function __construct(Zend_Navigation_Container $navigation, Traversable $iterator, $mode = self::SELF_FIRST, $flags = 0) {
+		$this->_navigation = $navigation;
+		parent::__construct($iterator, $mode, $flags);
+	}
+
+	/**
+	 * @return Zend_Navigation_Container
+	 */
+	public function create() {
+		$this->_parentPage = $this->_navigation;
+		while ($this->valid()) {
+			$current = $this->current()->toArray();
+			if (null !== $this->_visitor) {
+				$current = $this->_visitor->prepare($current);
+			}
+
+			$this->_currentPage = Zend_Navigation_Page::factory($current);
+			
+			$this->_parentPage->addPage($this->_currentPage);
+			$this->next();
+		}
+		return $this->_navigation;
+	}
+
+	/**
+	 * @var KontorX_Navigation_Recursive_Visitor
+	 */
+	private $_visitor = null;
+	
+	/**
+	 * @param KontorX_Navigation_Recursive_Visitor_Interface $visitor
+	 * @return KontorX_Navigation_Recursive
+	 */
+	public function accept(KontorX_Navigation_Recursive_Visitor_Interface $visitor) {
+		$this->_visitor = $visitor;
+		return $this;
+	}
+
+	private $_open = 0;
+	
+	public function beginChildren() {
+		++$this->_open;
+		$this->_parentPage = $this->_currentPage;
+//		$nav = $this->_navigation;
+//		$this->_navigation = new Zend_Navigation_Container();
+//		$nav->addPage($this->_navigation);
+//		Zend_Debug::dump($this->getInnerIterator()->toArray(),'inner');
+//		Zend_Debug::dump($this->current()->name,'begin');
+//		Zend_Debug::dump($this->_open,'begin');
+	}
+	
+	public function endChildren() {
+		--$this->_open;
+		$this->_parentPage = $this->_navigation;
+//		Zend_Debug::dump($this->key(),'end');
+//		Zend_Debug::dump($this->_open,'end');
+	}
+}
--- Promotor/View/Helper/SiteNavigation.php
+++ Promotor/View/Helper/SiteNavigation.php
@@ -1,32 +1,5 @@
 <?php
 class Promotor_View_Helper_SiteNavigation extends Zend_View_Helper_Navigation {
-	const PARTIAL = 'siteNavigation.phtml';
-	
-	/**
-	 * @var KontorX_Db_Table_Tree_Rowset_Abstract
-	 */
-	protected $_rowset = null;
-	
-	/**
-	 * @param KontorX_Db_Table_Tree_Rowset_Abstract $rowset
-	 * @return Promotor_View_Helper_SiteNavigation
-	 */
-	public function setRowset(KontorX_Db_Table_Tree_Rowset_Abstract $rowset) {
-		$this->_rowset = (string) $rowset;
-		return $this;
-	}
-
-	/**
-	 * @return KontorX_Db_Table_Tree_Rowset_Abstract
-	 * @throws Zend_Validate_Exception
-	 */
-	public function getRowset() {
-		if (null === $this->_rowset) {
-			throw new Zend_Validate_Exception('data rowset do not exsists');
-		}
-		return $this->_rowset;
-	}
-	
 	/**
 	 * @param string $proxy
 	 * @return Promotor_View_Helper_SiteNavigation
@@ -35,10 +8,10 @@ class Promotor_View_Helper_SiteNavigation extends Zend_View_Helper_Navigation {
 		if (null !== $proxy) {
 			$this->setDefaultProxy($proxy);
 		}
+
+		$model = new Site_Model_Site();
+		$container = $model->getNavigation();
+		$this->setContainer($container);
 		return $this;
-	}
-	
-	public function render() {
-		parent::render($container);
 	}
 }
