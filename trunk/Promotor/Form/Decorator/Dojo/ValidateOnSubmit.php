f17623ee8303e1a85e8ca7c2c399e981ccea8e4e
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-06-18 19:29:26 +0000

    Promotor_Form_Decorator_Dojo_ValidateOnSubmit

---
+++ trunk/Promotor/Form/Decorator/Dojo/ValidateOnSubmit.php
@@ -0,0 +1,34 @@
+<?php
+class Promotor_Form_Decorator_Dojo_ValidateOnSubmit extends Zend_Form_Decorator_Abstract {
+
+	protected $_onLoad = 'function(){
+		var myForm = dijit.byId("%s");
+		dojo.connect(myForm, "onSubmit", function(e){
+			e.preventDefault();
+			if (myForm.isValid()){
+				return true;
+			} else {
+				myForm.validate();
+			}
+		});
+	}';
+
+	public function render($content) {
+		$form = $this->getElement();
+		// only for Zend_Form
+		if ($form instanceof Zend_Form) {
+			$view = $form->getView();
+			if (null === ($id = $form->getId())) {
+				$id = 'form-' . uniqid();
+				$form->setAttrib('id',$id);
+			}
+	
+			$dojo = $view->getHelper('dojo');
+	
+			$onload = sprintf($this->_onLoad, $id);
+			$dojo->addOnLoad($onload);
+		}
+
+		return $content;
+	}
+}
