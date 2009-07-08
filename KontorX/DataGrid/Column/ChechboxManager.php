770731f4865bc2fbdb6d8a3174c231c5321e07b1
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-07-08 09:32:50 +0000

    add KontorX_DataGrid_Column_ChechboxManager

---
+++ KontorX/DataGrid/Column/ChechboxManager.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'KontorX/DataGrid/Column/Abstract.php';
+/**
+ * This column type required jQuery (http://www.jquery.com/)
+ *
+ * @author gabriel
+ */
+class KontorX_DataGrid_Column_ChechboxManager extends KontorX_DataGrid_Column_Abstract {
+	
+	public function render() {
+		return '<input type="checkbox" onclick="jQuery(\'input:checkbox\').attr(\'checked\', this.checked);"/>';
+	}
+}
