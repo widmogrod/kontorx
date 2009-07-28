4a74d1e227b613a9e48e805d12bc81557ec49ec6
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-07-28 12:26:47 +0000

    update absolute url view helper

--- trunk/KontorX/View/Helper/AbsoluteUrl.php
+++ trunk/KontorX/View/Helper/AbsoluteUrl.php
@@ -16,16 +16,34 @@ class KontorX_View_Helper_AbsoluteUrl implements Zend_View_Helper_Interface {
 	 */
 	protected $_baseUrl;
 	
+	/**
+	 * @var Zend_View_Helper_Url
+	 */
+	protected $_url;
+	
 	public function setView(Zend_View_Interface $view) {
 		$this->view = $view;
+		$this->_url = $view->getHelper('Url');
 		$this->_baseUrl = $view->getHelper('baseUrl')->getBaseUrl();
+	}
+
+	/**
+	 * @param array $params
+	 * @param string $router
+	 * @param bool $reset
+	 * @param bool $encode
+	 */
+	public function absoluteUrl(array $params, $router = null, $reset = false, $encode = true) {
+		return $this->_baseUrl . $this->_url->url($params, $router, $reset, $encode);
 	}
 	
 	/**
 	 * @param array $params
 	 * @param string $router
+	 * @param bool $reset
+	 * @param bool $encode
 	 */
-	public function absoluteUrl(array $params, $router = null) {
-		return $this->_baseUrl . $this->view->url($params, $router);
+	public function direct(array $params = array(), $router = null, $reset = false, $encode = true) {
+		return $this->absoluteUrl($params, $router, $reset, $encode);
 	}
 }
