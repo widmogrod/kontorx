38655112753cfc718f0f88cf01b554dd416c7c4f
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-08-23 19:51:33 +0000

    Dodane filtrowanie DataGrid_Adapter_Array .. i kilka zmian

--- trunk/KontorX/DataGrid/Filter/Order.php
+++ trunk/KontorX/DataGrid/Filter/Order.php
@@ -3,28 +3,34 @@ require_once 'KontorX/DataGrid/Filter/Abstract.php';
 class KontorX_DataGrid_Filter_Order extends KontorX_DataGrid_Filter_Abstract {
 
     public function filter(KontorX_DataGrid_Adapter_Interface $adapter) {
-        if (!$adapter instanceof KontorX_DataGrid_Adapter_DbTable
-        	&& !$adapter instanceof KontorX_DataGrid_Adapter_DbTableTree
-            && !$adapter instanceof KontorX_DataGrid_Adapter_DbSelect) {
-            require_once 'KontorX/DataGrid/Exception.php';
+        if ($adapter instanceof KontorX_DataGrid_Adapter_DbTable
+        		|| $adapter instanceof KontorX_DataGrid_Adapter_DbTableTree
+        		|| $adapter instanceof KontorX_DataGrid_Adapter_DbSelect)
+        {
+        	require_once 'KontorX/DataGrid/Filter/Order/Db.php';
+            $filter = new KontorX_DataGrid_Filter_Order_Db($this->getAttribs());
+        } elseif ($adapter instanceof KontorX_DataGrid_Adapter_Array) {
+        	require_once 'KontorX/DataGrid/Filter/Order/Array.php';
+        	$filter = new KontorX_DataGrid_Filter_Order_Array($this->getAttribs());
+        } else {
+        	require_once 'KontorX/DataGrid/Exception.php';
             throw new KontorX_DataGrid_Exception("Wrong filter adapter");
         }
 
-        $select = $adapter->getSelect();
-        $column = $this->getColumnName();
-        // set default order type
-        $order = $this->getValue($this->getAttrib('order'));
-
-        if (null !== $order) {
-            $order = ($order != 'asc') ? 'desc' : $order;
-            $select->order(sprintf('%s %s', $column, $order));
-        } else
-    	if ($this->getColumn()->isGroup()) {
-        	$select->order(sprintf('%s %s', $column, 'desc'));
-        }
+        // przekaż parametry
+        $filter->setClassName($this->getClassName());
+        $filter->setName($this->getName());
+        $filter->setColumn($this->getColumn());
+        $filter->setColumnName($this->getColumnName());
+        $filter->setValues($this->getValues());
+        $filter->setAttribs($this->getAttribs());
+        
+        // filtruj
+        $filter->filter($adapter);
     }
 
     public function render() {
+    	// filter nic nie renderuje
         return '';
     }
 }
