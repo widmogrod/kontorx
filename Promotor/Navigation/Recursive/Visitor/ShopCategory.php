a3713272b8ca8a6f9e7986f8dfbf5c652cb27e6d
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-10-09 10:25:16 +0000

    Promotr:Navigation_Recursive_Visitor_ShopCategory

---
+++ Promotor/Navigation/Recursive/Visitor/ShopCategory.php
@@ -0,0 +1,17 @@
+<?php
+class Promotor_Navigation_Recursive_Visitor_ShopCategory implements KontorX_Navigation_Recursive_Visitor_Interface {
+	public function prepare(array $current) {
+		$current['label'] = @$current['name'];
+
+		if (strlen(@$current['alias']) > 0) {
+			$params = array('alias' => $current['alias']);
+		} else {
+			$params	= array('alias' => $current['id']);
+		}
+		
+		$current['route'] = 'shop-category';
+		$current['params'] = $params;
+		
+		return $current;
+	}
+}
