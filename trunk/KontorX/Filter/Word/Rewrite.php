02b35be0ea474d1e9610ccc2971bab203d94a94e
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2009-10-16 09:29:02 +0000

    KontorX:update filter Word_Rewrite

--- trunk/KontorX/Filter/Word/Rewrite.php
+++ trunk/KontorX/Filter/Word/Rewrite.php
@@ -9,19 +9,16 @@ require_once 'Zend/Filter/Interface.php';
  * 
  * @category 	KontorX
  * @package 	KontorX_Filter
- * @version 	0.1.2
  * @license		GNU GPL
- * @author 		Marcin `widmogror` Habryn, widmogrod@gmail.com
  */
 class KontorX_Filter_Word_Rewrite implements Zend_Filter_Interface {
-	protected $_search   = array('á','â','ä','é','ë','í','î','ó','ô','ö','ú','ü','ý','ą','ę','ś','ź','ż','ń','ł','ć','-','/');
-	protected $_replace  = array('a','a','a','e','e','i','i','o','o','o','u','u','y','a','e','s','z','z','n','l','c','-','-');
+	protected $_search   = array('á','â','ä','é','ë','í','î','ó','ô','ö','ú','ü','ý','ą','ę','ś','ź','ż','ń','ł','ć');
+	protected $_replace  = array('a','a','a','e','e','i','i','o','o','o','u','u','y','a','e','s','z','z','n','l','c');
 
 	public function filter($value, $regxpAllow = null) {
 		$value = (string) $value;
-		$value = strtolower($value);
 		$value = str_replace($this->_search, $this->_replace, $value);
 		$regxpAllow = preg_quote($regxpAllow, '#');
-		return preg_replace("#[^a-z0-9/$regxpAllow]+#i", '-', $value);
+		return preg_replace("#[^\p{L}\p{N}$regxpAllow]+#u", '-', $value);
 	}
 }
