9a117f99d419382d1816b2c0caa83313e031dacd
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2010-03-03 14:15:42 +0000

    aktualizacja scaffold akcji Db_Table_Row - __clone() na rekordzie i sava() tworzy nowy rekord

--- KontorX/Db/Table/Row.php
+++ KontorX/Db/Table/Row.php
@@ -1,5 +1,27 @@
 <?php
 require_once 'Zend/Db/Table/Row/Abstract.php';
-class KontorX_Db_Table_Row extends Zend_Db_Table_Row_Abstract {
+class KontorX_Db_Table_Row extends Zend_Db_Table_Row_Abstract
+{
+	public function __clone() 
+	{
+		if (!is_array($this->_primary)) {
+			return;
+		}
 
+		if (empty($this->_cleanData)) {
+			return;
+		}
+
+		/**
+		 * Poniższe opcje maja zapewnic że po wykonaniu na
+		 * sklonowanym obiekcie ->save() zostanie dodany nowy rekord!
+		 */
+		
+		// zapewnia insert
+		$this->_cleanData = null;
+		// usunięcie informacji o primary key
+		$this->_data = array_diff_key($this->_data, array_flip($this->_primary));
+		// pola modyfikowalne ustaw na wszystkie!
+		$this->_modifiedFields = $this->_data;
+	}	
 }
