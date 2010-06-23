1cb1fac7bfafa82023e2b34efd8f09c41bb2b9c3
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2010-06-23 15:20:24 +0000

    Dodanie przykładów DatGrid

---
+++ trunk/examples/index.php
@@ -0,0 +1,15 @@
+<?php
+//inicjowanie konfiguracji
+require_once 'bootstrap.php';
+
+// TODO Budowanie drzewa zaleznoś
+
+//$path = dirname(__FILE__);
+//$rdi = new RecursiveDirectoryIterator($path);
+//$rii = new RecursiveIteratorIterator($rdi, RecursiveIteratorIterator::SELF_FIRST);
+
+//require_once 'KontorX/Navigation/Recursive.php';
+//$navigation = new KontorX_Navigation_Recursive($rdi);
+//require_once 'Promotor/Navigation/Recursive/Visitor/Site.php';
+//$navigation->accept(new Promotor_Navigation_Recursive_Visitor_Site());
+//print $navigation->create(); // Zend_Navigation_Container
