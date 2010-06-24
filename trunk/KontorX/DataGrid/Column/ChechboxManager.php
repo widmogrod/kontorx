4b52015179386e8553b219c51b41cfa87baf649a
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2010-06-24 11:31:25 +0000

    Dodanie nowego przykładu do DataGrid

--- trunk/KontorX/DataGrid/Column/ChechboxManager.php
+++ trunk/KontorX/DataGrid/Column/ChechboxManager.php
@@ -1,9 +1,12 @@
 <?php
 require_once 'KontorX/DataGrid/Column/Abstract.php';
+
 /**
  * This column type required jQuery (http://www.jquery.com/)
- *
+ * Requirements:
+ * - JQuery
  * @author gabriel
+ * 
  */
 class KontorX_DataGrid_Column_ChechboxManager extends KontorX_DataGrid_Column_Abstract {
 	
