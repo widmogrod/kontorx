b7ae2b4fa1a41dc621ccb7452b3a51dec14d7d57
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2010-07-08 08:49:09 +0000

    Aktualizacja Products i Page

--- KontorX/Sisi/Action/Page.php
+++ KontorX/Sisi/Action/Page.php
@@ -1,6 +1,49 @@
 <?php
 require_once 'KontorX/Sisi/Action/Interface.php';
 
+class Pages
+{
+	protected $_activePageId;
+	
+	protected $_pages;
+	
+	protected $_pagePathname;
+	
+	/**
+	 * @param string $pagePathname
+	 */
+	public function __construct($pagePathname) {
+		if (!is_dir($pagePathname))
+			throw new Exception(sprintf('Katalog z stronami nie istnieje "%s"', $pagePathname));
+		
+		$this->_pagePathname = rtrim((string)$pagePathname,'/');
+	}
+
+	public function setActiveId($id) {
+		$this->_activePageId = basename((string) $id);
+	}
+	
+	public function getActiveId() {
+		return $this->_activePageId;
+	}
+	
+	public function getPage($pageId = null) {
+		if (null !== $pageId)
+			$pageId = $this->getActivePageId();
+
+		$pageId = basename($pageId);
+
+		$path = $this->_pagePathname . '/' . $pageId;
+		if (!is_file($path)) {
+			return;
+		}
+
+		ob_start();
+		include $path;
+		return ob_get_clean();
+	}
+}
+
 class KontorX_Sisi_Action_Page implements KontorX_Sisi_Action_Interface
 {
 	/**
@@ -13,6 +56,14 @@ class KontorX_Sisi_Action_Page implements KontorX_Sisi_Action_Interface
     		$response->setScriptName('index');
     	}
 
-    	$page = $sisi->getParam('page','index');
+    	$pages = new Pages(PAGE_PATHNAME);
+    	$pages->setActiveId($sisi->getParam('id','index'));
+    	$page = $pages->getPage();
+
+    	$result = array(
+    		'page' => $page,
+    	);
+
+    	$response->setData($result);
     }
 }
