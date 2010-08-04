7499d113586836bd38ec326147c1fe52f046d716
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2010-08-04 09:58:54 +0000

    Aktualizacja PrevNext itp

--- trunk/Promotor/View/Helper/SiteNavigation.php
+++ trunk/Promotor/View/Helper/SiteNavigation.php
@@ -1,18 +1,35 @@
 <?php
-class Promotor_View_Helper_SiteNavigation extends Zend_View_Helper_Navigation {
+class Promotor_View_Helper_SiteNavigation extends Zend_View_Helper_Abstract 
+{
+	/**
+	 * @return Zend_View_Helper_Navigation
+	 */
+	public function getNavigation() 
+	{
+		return $this->view->getHelper('navigation');
+	}
+
 	/**
 	 * @param string $proxy
 	 * @return Promotor_View_Helper_SiteNavigation
 	 */
-	public function siteNavigation($proxy = null) {
-		if (null !== $proxy) {
-			$this->setDefaultProxy($proxy);
-		}
-
+	public function siteNavigation($proxy = null) 
+	{
 		$model = new Site_Model_Site();
 		$container = $model->getNavigation();
 		
-		$this->setContainer($container);
-		return $this;
+		$navigation = $this->getNavigation();
+		$navigation->setContainer($container);
+		
+		if (null !== $proxy)
+			$navigation->setDefaultProxy($proxy);
+
+		return $navigation;
+	}
+
+	public function __call($method, $params)
+	{
+		$navigation = $this->getNavigation();
+		return call_user_func_array(array($navigation, $method), $params);
 	}
 }
