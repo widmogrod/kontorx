b02e1ec166e6583e7df157f2898c497dd940c5b8
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2010-08-11 10:37:07 +0000

    Drobne poprawki w walidatorze NIP

--- KontorX/Form/Element/NIP.php
+++ KontorX/Form/Element/NIP.php
@@ -1,7 +1,8 @@
 <?php
-class KontorX_Form_Element_NIP extends Zend_Form_Element_Xhtml {
-
-	public function init() {
+class KontorX_Form_Element_NIP extends Zend_Form_Element_Xhtml 
+{
+	public function init() 
+	{
 		$this->addPrefixPath(
 			'KontorX_Form_Decorator',
 			'KontorX/Form/Decorator',
@@ -18,7 +19,8 @@ class KontorX_Form_Element_NIP extends Zend_Form_Element_Xhtml {
 		$this->addValidator('NIP');
 	}
 	
-	public function loadDefaultDecorators() {
+	public function loadDefaultDecorators() 
+	{
 		if ($this->loadDefaultDecoratorsIsDisabled()) {
             return;
         }
@@ -37,7 +39,8 @@ class KontorX_Form_Element_NIP extends Zend_Form_Element_Xhtml {
 	/**
 	 * @return string
 	 */
-	public function getValue() {
+	public function getValue() 
+	{
 		$value = (array) parent::getValue();
 		return implode($value);
 	}
@@ -45,9 +48,36 @@ class KontorX_Form_Element_NIP extends Zend_Form_Element_Xhtml {
 	/**
 	 * @return array:
 	 */
-	public function getNIPParts() {
-		$value = (array) parent::getValue();
-		$value += array_fill(0,4,null);
-		return $value;
+	public function getNIPParts() 
+	{
+		$result = array(
+			'   ',
+			'   ',
+			'  ',
+			'  '
+		);
+		
+		$value = parent::getValue();
+
+		// usuń wszystkie znaki, które nie są cyfrą
+		if (!is_numeric($value) && is_string($value))
+			$value = preg_replace('/^[0-9]+/','', $value);
+		
+		// jeżeli jest to liczba potnij ją w odpowiednim formacie NIP
+		if (is_numeric($value)) 
+		{
+			$position = 0;
+			foreach ($result as $key => $part) {
+				$length = strlen($part);
+
+				$result[$key] = substr($value, $position, $length);
+				$position += $length;
+			}
+		} else {
+			$result = (array) $value;
+		}
+
+		$result += array_fill(0,4,null);
+		return $result;
 	}
 }
