e44fae421ef8c553e93cad8c09ac3d0ddd1089a2
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2010-08-11 08:17:21 +0000

    Aktualizacja i porzatki w aktualizacji generatora PDFów

--- KontorX/Pdf.php
+++ KontorX/Pdf.php
@@ -28,7 +28,7 @@ class KontorX_Pdf
 		if (is_string($adapter)) {
 			$adapter = ucfirst($adapter);
 			$class = $instance->getPluginLoader()->load($adapter);
-			$adapter = new $class($options);
+			$adapter = new $class();
 		}
 
 		if (!$adapter instanceof KontorX_Pdf_Adapter_Abstract) {
@@ -51,15 +51,20 @@ class KontorX_Pdf
 	{}
 	
 	/**
+	 * Konfiguruj główną klasę jak i adapter
 	 * @param array $options
 	 */
 	public function setOptions(array $options)
 	{
+		$adapter = $this->getAdapter();
 		foreach ($options as $key => $value) 
 		{
 			$methodName = 'set' . ucfirst($key);
-			if (method_exists($this, $methodName))
+			if (method_exists($this, $methodName)) {
 				$this->$methodName($value);
+			} elseif (method_exists($adapter, $methodName)) {
+				$adapter->$methodName($value);
+			}
 		}
 	}
 	
@@ -115,18 +120,88 @@ class KontorX_Pdf
     }
 
     /**
+     * @var string
+     */
+    protected $_fileame;
+	
+	/**
+	 * Ustawienie nazwy jaką nazwę ma posiadać wysyłany do pobrania plik 
+	 * podczas wywołania metody @see KontorX_Pdf::output()
+	 * 
+	 * @param string $fileame
+	 */
+	public function setFilename($fileame)
+	{
+		$this->_fileame = $fileame;
+	}
+	
+	/**
+	 * Pobranie nazwy pliku
+	 * @return string
+	 */
+	public function getFilename()
+	{
+		if (null === $this->_fileame)
+		{
+			return 'document.pdf';
+		}
+
+		return $this->_fileame;
+	}
+    
+    
+    /**
      * @param string $html
      */
     public function setHtml($html)
     {
     	$this->getAdapter()->setHtml($html);
     }
-    
+
+    /**
+     * Zwraca ścieżkę do dokumentu PDF, który został wyrenderowany
+     * @return string
+     */
+    public function render() 
+    {
+    	$adapter = $this->getAdapter();
+    	$adapter->render();
+    	$filepath = $adapter->getOutputFilepath();
+    	return $filepath;
+    }
+
     /**
      * @void
      */
     public function output()
     {
-    	$this->getAdapter()->output();
+    	if(headers_sent()) {
+			$message = 'Headers were already sent.';
+			require_once 'KontorX/Pdf/Exception.php';
+			throw new KontorX_Pdf_Exception($message);
+		}
+
+		$adapter = $this->getAdapter();
+    	$adapter->render();
+
+    	$filepath = $adapter->getOutputFilepath();
+		$filename = $this->getFilename();
+
+		header('Content-Description: File Transfer');
+		header('Cache-Control: public, must-revalidate, max-age=0'); // HTTP/1.1
+		header('Pragma: public');
+		header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+		header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+		// force download dialog
+		header('Content-Type: application/force-download');
+		header('Content-Type: application/octet-stream', false);
+		header('Content-Type: application/download', false);
+		header('Content-Type: application/pdf', false);
+		// use the Content-Disposition header to supply a recommended filename
+		header('Content-Disposition: attachment; filename="'.basename($filename).'";');
+		header('Content-Transfer-Encoding: binary');
+		header('Content-Length: '.filesize($filepath));
+
+		readfile($filepath);
     }
 }
--- KontorX/Pdf/Adapter/Abstract.php
+++ KontorX/Pdf/Adapter/Abstract.php
@@ -1,50 +1,132 @@
 <?php
 abstract class KontorX_Pdf_Adapter_Abstract
 {
-	abstract public function output();
+	/**
+	 * Renderowanie dokumentu HTML do PDF
+	 * @return void
+	 */
+	abstract public function render();
 
-	public function __construct(array $options = null)
-	{
-		if (is_array($options))
-			$this->setOptions($options);
-	}
-	
-	public function setOptions(array $options)
-	{
-		foreach ($options as $key => $value) 
-		{
-			$methodName = 'set' . ucfirst($key);
-			if (method_exists($this, $methodName))
-				$this->$methodName($value);
-		}
-	}
-	
-	protected $_fileame;
-	
-	public function setFilename($fileame)
-	{
-		$this->_fileame = $fileame;
-	}
-	
-	public function getFilename()
-	{
-		if (null === $this->_fileame)
-		{
-			return 'document.pdf';
-		}
-
-		return $this->_fileame;
-	}
-
+	/**
+	 * @var string
+	 */
 	protected $_html;
 	
+	/**
+	 * Ustaw HTML
+	 * @param striing $html
+	 */
 	public function setHtml($html)
 	{
 		$this->_html = $html;
 	}
 	
+	/**
+	 * Pobierz HTML
+	 * @return string
+	 */
 	public function getHtml()
 	{
 		return $this->_html;
+	}
+	
+	/**
+	 * Ścieżka do katalogu z plikami tymczasowymi
+	 * @var string
+	 */
+	protected $_tempDir;
+	
+	/**
+	 * Ścieżka do katalogu w którym mają być przechowywane tymczasowe pliki
+	 * potrzebne do wygenerowania dokumentu PDF
+	 * 
+	 * @param string $tempDir
+	 */
+	public function setTempDir($tempDir)
+	{
+		$this->_tempDir = $tempDir;
+	}
+	
+	/**
+	 * Zwaca ścieżkę do katalogu przechowywującego pliki tymczasowe
+	 * 
+	 * Jeżeli nie zostanie zdefiniowany katalog TEMP to jest ustawiany
+	 * jako domyślny systemowy katalog TEMP 
+	 * 
+	 * @return string
+	 */
+	public function getTempDir()
+	{
+		if (null === $this->_tempDir)
+			$this->_tempDir = sys_get_temp_dir();
+
+		return $this->_tempDir;
+	}
+
+	/**
+	 * @var string
+	 */
+	protected $_inputFilepath;
+	
+	/**
+	 * Ustaw ścieżkę do pliku, który jest dokumentem HTML,
+	 * który ma zostać uzyty do wygenerowania dokumentu PDF
+	 * 
+	 * @param string $inputFilepath
+	 */
+	public function setInputFilepath($inputFilepath)
+	{
+		$this->_inputFilepath = $inputFilepath;
+	}
+	
+	/**
+	 * Zwraca ścieżkę do pliku, który ma przechowywać HTML
+	 *
+	 * Jeżeli nie zostanie podana nazwa pliku, 
+	 * zostanie wygenerowana nazwa pliku.
+	 * 
+	 * @return string
+	 */
+	public function getInputFilepath()
+	{
+		if (null === $this->_inputFilepath) {
+			$this->_inputFilepath = tempnam($this->getTempDir(), get_class($this));
+			$this->_inputFilepath .= '.html';
+		}
+
+		return $this->_inputFilepath;
+	}
+	
+	/**
+	 * @var string
+	 */
+	protected $_outputFilepath;
+	
+	/**
+	 * Ścieżka do pliku gdzie ma być zapisany wygenerowany dokument PDF
+	 * 
+	 * @param string $outputFilepath
+	 */
+	public function setOutputFilepath($outputFilepath)
+	{
+		$this->_outputFilepath = $outputFilepath;
+	}
+	
+	/**
+	 * Zwróć ścieżkę do pliku, który ma przechowywać dokument PDF
+	 * 
+	 * Jeżeli nazwa pliku nie została wcześniej podana 
+	 * to zostabie wygenerowana nazwa automatycznie
+	 *  
+	 * @return string
+	 */
+	public function getOutputFilepath()
+	{
+		if (null === $this->_outputFilepath) {
+			$this->_outputFilepath = $this->getInputFilepath();
+			$this->_outputFilepath .= '.pdf';
+		}
+
+		return $this->_outputFilepath;
 	}
 }
