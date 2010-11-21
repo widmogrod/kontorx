42cab238b0483380e0fab74440ebadfc15845bab
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2010-11-21 23:38:55 +0000

    Aktualizacja klasy sprawdzającej pozycję słowa kluczowego w Google.pl z możliwością sprawdzania proxy i aktualizacji stanu proxy

--- KontorX/Util/Google.php
+++ KontorX/Util/Google.php
@@ -1,31 +1,112 @@
 <?php
 /**
- * 
+ * Klasa sprawdzająca pozycję strony w Google.pl
+ * Miłym dodatkiem jest sprawdzanie pozycji z różnych adresów proxy.
+ *
+ * @author Gabriel Habryn, widmogrod.info
+ * @version $Id$
  */
-class KontorX_Util_Google {
+class KontorX_Util_Google 
+{
 	private
-		$sUri = null;
-
-	public function __construct( $sUrl ){
+		$sUri = null,
+		$iCount = 100,
+		$sWord = null,
+		$aProxies = array(),
+		$aProxiesFaild = array(),
+		$sProxiesFile = null;
+		
+	public function __construct($sUrl)
+	{
 		$this->sUri = $sUrl;
 	}
+	
+	public function addProxy($proxy)
+	{
+		$this->aProxies[] = $proxy;
+	}
 
-	public function position( $sWord, $iCount = null ){
-		$iCount = is_integer( $iCount )
+	public function setProxy($proxy)
+	{
+		$this->aProxies = array($proxy);
+		$this->aProxiesFaild = array();
+	}
+	
+	public function setProxies(array $proxies)
+	{
+		$this->aProxies = $proxies;
+		$this->aProxiesFaild = array();
+	}
+
+	public function setProxiesFile($file)
+	{
+		$this->sProxiesFile = $file;
+
+		$proxies = array();
+		$proxiesFails = array();
+
+		$handle = fopen($this->sProxiesFile, 'r');
+		while ($userinfo = fscanf($handle, "%s\t%d\n")) {
+		    list ($proxies[], $proxiesFails[]) = $userinfo;
+		}
+		fclose($handle);
+
+		$proxies = array_unique($proxies);
+		$this->setProxies($proxies);
+		
+		$this->aProxiesFaild = $proxiesFails;
+	}
+	
+	public function saveProxiesInfoToFile()
+	{
+
+		if (null === $this->sProxiesFile)
+			return;
+
+		$handle = fopen($this->sProxiesFile, 'w');
+		foreach($this->aProxies as $key => $proxie)
+		{
+			$fails = array_key_exists($key, $this->aProxiesFaild)
+				? $this->aProxiesFaild[$key] : 0;
+
+			fwrite($handle, sprintf("%s\t%d\n", $proxie, $fails));
+		}
+
+		fclose($handle);
+	}
+
+	public function position($sWord, $iCount = null)
+	{
+		$this->sWord = $sWord;
+		$this->iCount = is_integer($iCount)
 			? $iCount : 100;
 
-		// laczenie z google
-		$rCurl = curl_init();
+		if (count($this->aProxies))
+		{
+			// while ($sProxy = array_pop($this->aProxies)) 
+			foreach($this->aProxies as $key => $sProxy)
+			{
+				$sData = $this->getData($sProxy);
+				if (false !== $sData) {
+					$this->saveProxiesInfoToFile();
+					break;
+				} else {
+					if (!array_key_exists($key, $this->aProxiesFaild)) {
+						$this->aProxiesFaild[$key] = 0;
+					}
 
-		curl_setopt($rCurl, CURLOPT_HEADER, 0);
-		curl_setopt($rCurl, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($rCurl, CURLOPT_VERBOSE, 1);
-		curl_setopt($rCurl, CURLOPT_REFERER, 'www.google.pl');
-		curl_setopt($rCurl, CURLOPT_URL, sprintf( 'http://www.google.pl/search?hl=pl&q=%s&num='.$iCount, urlencode( $sWord )));
+					++$this->aProxiesFaild[$key];					
+				}
+			}
+			if (false === $sData) {
+				$sData = $this->getData();
+			}
+		} else {
+			$sData = $this->getData();
+		}
 
-		$sData = curl_exec($rCurl);
-
-		curl_close($rCurl);
+		if (!$sData)
+			return false;
 
 		// lapiemy linki
 		// TODO Dodać lapanie w wynikach pola z pdf etc.
@@ -40,21 +121,60 @@ class KontorX_Util_Google {
 
 		// lementy pod kluczem 1 zawieraja tylko linki do strony
 		$aResults = (array) @$aResults[1];
+		
+		if (count($aResults) == 0) {
+			return false;
+		}
 
 		$aReturn = array($this->sUri => false, $sUri=>false);
 
 		// szuka pozycje w google.pl
-		foreach( $aResults as $iKey => $sRowUrl ){
-			$sString = strip_tags( $sRowUrl );
-			if( strpos( $sString, $this->sUri ) !== false)
+		foreach($aResults as $iKey => $sRowUrl)
+		{
+			$sString = strip_tags($sRowUrl);
+			if(strpos($sString, $this->sUri) !== false)
 				$aReturn[$this->sUri] = $iKey+1;
 
-			if(  strpos( $sString, $sUri ) !== false )
+			if(strpos($sString, $sUri) !== false)
 				$aReturn[$sUri] = $iKey+1;
 		}
 
 		// zwroc najwieksza pozycje
 		arsort($aReturn);
 		return (int) array_shift( $aReturn );
+	}
+	
+	public function getData($sProxy = null)
+	{
+		// laczenie z Google.pl
+		$rCurl = curl_init();
+
+		curl_setopt($rCurl, CURLOPT_HEADER, 0);
+		curl_setopt($rCurl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($rCurl, CURLOPT_VERBOSE, 0);
+		curl_setopt($rCurl, CURLOPT_REFERER, 'www.google.pl');
+		curl_setopt($rCurl, CURLOPT_URL, sprintf( 'http://www.google.pl/search?hl=pl&q=%s&num='.$this->iCount, urlencode($this->sWord)));
+
+		if (null !== $sProxy) {
+			curl_setopt($rCurl, CURLOPT_PROXY, $sProxy);
+		}
+
+		$sData = curl_exec($rCurl);
+		
+		if (0 != curl_errno($rCurl)) 
+		{
+			curl_close($rCurl);
+			return false;			
+		}
+
+		// Jeżeli jest odpowiedź jest mniejsza niż 5000 znaków
+		// oznacza to że że odpowiedź z google nie zawiera wyników wyszukiwania
+		// tylko mechanizm ochronny googla przed nadmiernym pingowaniem
+		if (strlen($sData) < 5000)
+			return false;
+
+		curl_close($rCurl);
+		
+		return $sData;
 	}
 }
