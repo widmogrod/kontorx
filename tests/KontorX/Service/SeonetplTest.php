a573d9bb3605b38766afe314597536708d620790
widmogrod <widmogrod@f872f0db-dc53-0410-9f59-3dfad7ff53de>
2011-01-06 23:59:51 +0000

    KontorX_Service_Seonetpl - nowa klasa umożliwiająca zarządzanie swoim kontem na seo.net.pl

--- tests/KontorX/Service/SeonetplTest.php
+++ tests/KontorX/Service/SeonetplTest.php
@@ -45,7 +45,7 @@ class KontorX_Service_SeonetplTest extends UnitTestCase {
 	public function testGetLinkSetSuccess() 
 	{	
 		$this->_seonetpl->authorise();
-		$result = $this->_seonetpl->getLinkSet();
+		$result = $this->_seonetpl->getLinkSets();
 		
 		$this->assertTrue(is_array($result), 'There is no link sets for user');
 	}
@@ -53,7 +53,7 @@ class KontorX_Service_SeonetplTest extends UnitTestCase {
 	public function testGetExportDataForFirstLinkSet() 
 	{	
 		$this->_seonetpl->authorise();
-		$links = $this->_seonetpl->getLinkSet();
+		$links = $this->_seonetpl->getLinkSets();
 		
 		$result = false;
 		$linkSetId = null;
@@ -70,12 +70,80 @@ class KontorX_Service_SeonetplTest extends UnitTestCase {
 	public function testGetExportDataForCustomLinkSet() 
 	{	
 		$this->_seonetpl->authorise();
-		$links = $this->_seonetpl->getLinkSet();
+		$links = $this->_seonetpl->getLinkSets();
 
 		$linkSetId = 91794; // 2rsystem.com.pl
 		$result = $this->_seonetpl->exportDataForLinkSet($linkSetId);
 
 		$this->assertTrue(is_array($result), 'There is no export data for user link set = '.$linkSetId);
+	}
+	
+//	public function testSetExportDataForCustomLinkSet() 
+//	{	
+//		$this->_seonetpl->authorise();
+//		$links = $this->_seonetpl->getLinkSet();
+//
+//		$linkSetId = 91794; // 2rsystem.com.pl
+//		$data = $this->_seonetpl->exportDataForLinkSet($linkSetId);
+//		$result = $this->_seonetpl->importDataForLinkSet($data, $linkSetId);
+//
+//		$this->assertTrue($result, 'Failed importing data for link set = '.$linkSetId);
+//	}
+
+	public function testGetSetLinksSuccess() 
+	{	
+		$linkSetId = 91794; // 2rsystem.com.pl
+		
+		$result = $this->_seonetpl->getSetLinks($linkSetId);
+		$this->assertTrue(is_array($result), 'There is no link sets for user');
+	}
+	
+	public function testSetLinksMaxSuccess() 
+	{	
+		$max = 3000;
+		$linkId = 5441591; // inteligentny bdynek
+		$linkSetId = 91794; // 2rsystem.com.pl
+
+		$result = $this->_seonetpl->setLinksMax($max, $linkId, $linkSetId);
+		$this->assertTrue($result, 'Can\'t set $max do link');
+	}
+	
+	public function testSetLinksPerdaySuccess() 
+	{	
+		$perday = 20;
+		$linkId = 5441591; // inteligentny bdynek
+		$linkSetId = 91794; // 2rsystem.com.pl
+
+		$result = $this->_seonetpl->setLinksPerday($perday, $linkId, $linkSetId);
+		$this->assertTrue($result, 'Can\'t set perday do link');
+	}
+	
+	public function testSetLinksPrioritySuccess() 
+	{	
+		$priority = 10;
+		$linkId = 5441591; // inteligentny bdynek
+		$linkSetId = 91794; // 2rsystem.com.pl
+
+		$result = $this->_seonetpl->setLinksPriority($priority, $linkId, $linkSetId);
+		$this->assertTrue($result, 'Can\'t set $priority do link');
+	}
+	
+//	public function testDeleteLinkSuccess() 
+//	{	
+//		$linkId = 6696314;
+//		$linkSetId = 91794; // 2rsystem.com.pl
+//
+//		$result = $this->_seonetpl->deleteLink($linkId, $linkSetId);
+//		$this->assertTrue($result, 'Can\'t delete link');
+//	}
+	
+	public function testDeleteLinkFailure() 
+	{	
+		$linkId = 123321123;
+		$linkSetId = 91794; // 2rsystem.com.pl
+
+		$result = $this->_seonetpl->deleteLink($linkId, $linkSetId);
+		$this->assertFalse($result, 'Can\'t delete link');
 	}
 }
 
